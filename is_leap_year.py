121517eae79241bc2ff8ae88ee1661fc1648cf95
HeavyWeather <delta.thrives@gmail.com>
2014-10-31 16:13:46 +0000

    Create is_leap_year.py

---
+++ is_leap_year.py
@@ -0,0 +1,8 @@
+def is_leap_year(year):
+  """ is the specified year a leap year? """
+    if year % 100 != 0 and year % 4 == 0:
+        return True
+    elif year % 100 == 0 and year % 400 == 0:
+        return True
+    else:
+        return False
